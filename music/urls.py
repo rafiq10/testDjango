033d3b02f5a5a4726996b7990b1b60a4d6ccda31
rafiq10 <rafiq10@gmail.com>
2018-01-05 23:02:18 +0100

    32-beforeStart

--- music/urls.py
+++ music/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'music'
 
 urlpatterns = [
-    path('',views.index, name = 'music'),
-    re_path(r'^(?P<album_id>[0-9]+)/$',views.detail,name = 'details'),
-    re_path(r'^(?P<album_id>[0-9]+)/favorite', views.favorite,name = 'favorite'),
+    path('',views.IndexView.as_view(), name = 'music'),
+    re_path(r'^(?P<pk>[0-9]+)/$',views.DetailView.as_view(),name = 'details'),
+    re_path(r'album/add/$', views.AlbumCreate.as_view(), name = 'album-add'),
 ]
--- music/views.py
+++ music/views.py
@@ -1,27 +1,18 @@
-from django.http import Http404
-from .models import Album, Song
-from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Album
 
-def index(request):
-    all_albums = Album.objects.all()
-    context = {'all_albums': all_albums}
-    return render(request,'music/index.html',context)
+class IndexView(generic.ListView):
+    template_name = 'music/index.html'
+    context_object_name = 'all_albums'
 
-def detail(request,album_id):
-    album = get_object_or_404(Album, pk=album_id)
-    return render(request,'music/detail.html',{'album' : album})
+    def get_queryset(self):
+        return Album.objects.all()
 
-def favorite(request,album_id):
-    album = get_object_or_404(Album, pk=album_id)
-    try:
-        selected_song = album.song_set.get(pk=request.POST['song'])
-    except (KeyError,Song.DoesNotExist):
-        return render(request, 'music/detail.html', {
-        'album': album,
-        'error_message': 'you did not select a valid song',
-    })
-    else:
-        selected_song.is_favorite = True
-        selected_song.save()
-    
-    return render(request,'music/detail.html',{'album' : album})
+class DetailView(generic.DetailView):
+    model = Album
+    template_name = 'music/detail.html'
+
+class AlbumCreate(CreateView):
+    model = Album
+    fields = ['artist', 'album_title', 'genre','album_logo']
